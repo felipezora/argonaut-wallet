c3e677b1c511634d668e668cbaf1b8feb46884ba
Juan Zora <juanfzz2001@gmail.com>
2023-05-31 13:04:11 -0500

    test: finished wallet contract tests

--- test/Wallet.ts
+++ test/Wallet.ts
@@ -1,20 +1,26 @@
-/*
-List of tests:
-1. Owner equals to deployer of the contract => DONE
-2. validTokenInstances[0] equals to address passed as argument when deployed => DONE
-4. queryToken(0) returns token passed as arg when deployed => DONE
-5. queryToken(invalid index) should revert with message Invalid index => DONE
-6. registerToken as a common user should revert with message Not owner => DONE
-7. registerToken as owner and then queryToken previously registered
-8. getBalance(0) should return balance when called from owner account
-9. transfer to account and then getBalance from account receiving the balance
-*/
-
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
 describe("Wallet", () => {
+
   const deployWalletWithBNB = async () => {
+
+    // In hardhat, contracts are deployed using the first signer/account by default
+    const [owner] = await ethers.getSigners();
+
+    const tokenName = "BNB";
+    const tokenSymbol = "BNB";
+    const BNBToken = await ethers.getContractFactory("BNBToken");
+    const bnbInstance = await BNBToken.deploy(tokenName, tokenSymbol, { value: 0 });
+
+    const Wallet = await ethers.getContractFactory("Wallet");
+    const walletInstance = await Wallet.deploy(bnbInstance.address, { value: 0 });
+    return { owner, bnbInstance, walletInstance, tokenName, tokenSymbol };
+  }
+
+  const deployWalletWithBNBAndConnectOther = async () => {
+
+    // In hardhat, contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
     const tokenName = "BNB";
@@ -24,17 +30,23 @@ describe("Wallet", () => {
 
     const Wallet = await ethers.getContractFactory("Wallet");
     const walletInstance = await Wallet.deploy(bnbInstance.address, { value: 0 });
-    return { owner, otherAccount, bnbInstance, walletInstance, tokenName, tokenSymbol };
+
+    const walletInstanceFromOther = await ethers.getContractAt("Wallet", walletInstance.address, otherAccount);
+
+    return { owner, otherAccount, bnbInstance, walletInstance, walletInstanceFromOther, tokenName, tokenSymbol };
   }
 
   describe("Deployment", () => {
+
     it("Should set the right owner", async () => {
+
       const { owner, walletInstance } = await loadFixture(deployWalletWithBNB);
 
       expect(await walletInstance.owner()).to.equal(owner.address);
     });
 
     it("Should set the right address in the first position of validTokenInstances", async () => {
+
       const { bnbInstance, walletInstance } = await loadFixture(deployWalletWithBNB);
 
       expect(await walletInstance.validTokenInstances(0)).to.equal(bnbInstance.address);
@@ -42,15 +54,20 @@ describe("Wallet", () => {
   });
 
   describe("Query Token Validations", () => {
+
     it("Should query first token instance's info", async () => {
-      const { walletInstance, tokenName } = await loadFixture(deployWalletWithBNB);
+
+      const DEFAULT_BNB_DECIMALS = 18;
+
+      const { walletInstance, tokenName, tokenSymbol } = await loadFixture(deployWalletWithBNB);
 
       const tokenInfoArray = await walletInstance.queryToken(0);
 
-      expect(tokenInfoArray.name).to.equal(tokenName);
+      expect(tokenInfoArray).to.include.members([tokenName, tokenSymbol, DEFAULT_BNB_DECIMALS]);
     });
 
     it("Should revert with Invalid index", async () => {
+
       const { walletInstance } = await loadFixture(deployWalletWithBNB);
 
       await expect(walletInstance.queryToken(1)).to.be.revertedWith("Invalid index");
@@ -60,12 +77,73 @@ describe("Wallet", () => {
   describe("Register Token Validations", () => {
 
     it("Should revert with Not owner", async () => {
-      const { walletInstance, otherAccount } = await loadFixture(deployWalletWithBNB);
-      const anyAddress = "0x0000000000000000000000000000000000000000";
 
-      const walletInstanceFromOther = await ethers.getContractAt("Wallet", walletInstance.address, otherAccount);
+      const { walletInstanceFromOther } = await loadFixture(deployWalletWithBNBAndConnectOther);
 
-      await expect(walletInstanceFromOther.registerToken(anyAddress)).to.be.revertedWith("Not owner");
+      const randomAddress = "0x0000000000000000000000000000000000000000";
+
+      await expect(walletInstanceFromOther.registerToken(randomAddress)).to.be.revertedWith("Not owner");
+    });
+
+    it("Should register a new validTokenInstance and query it", async () => {
+
+      const { walletInstance } = await loadFixture(deployWalletWithBNB);
+
+      const DEFAULT_BNB_DECIMALS = 18;
+
+      const tokenName = "TEST";
+      const tokenSymbol = "TEST";
+      const TESTToken = await ethers.getContractFactory("BNBToken");
+      const testInstance = await TESTToken.deploy(tokenName, tokenSymbol, { value: 0 });
+
+      await walletInstance.registerToken(testInstance.address);
+
+      const testTokenInstanceInfo = await walletInstance.queryToken(1);
+
+      expect(testTokenInstanceInfo).to.include.members([tokenName, tokenSymbol, DEFAULT_BNB_DECIMALS]);
+    });
+  });
+
+  describe("Get Balance validations", () => {
+
+    it("Should get the BNB deployer account's BNB balance", async () => {
+
+      const BNBInitialSupply = ethers.BigNumber.from("100000000000000000000");
+
+      const BNB_INSTANCE_INDEX = 0;
+
+      const { walletInstance } = await loadFixture(deployWalletWithBNB);
+
+      expect(await walletInstance.getBalance(BNB_INSTANCE_INDEX)).to.deep.equal(BNBInitialSupply);
+    });
+
+    it("Should get the BNB balance of a random account", async () => {
+
+      const { walletInstanceFromOther } = await loadFixture(deployWalletWithBNBAndConnectOther);
+
+      const BNB_INSTANCE_INDEX = 0;
+
+      const expectedRandomAccountBalance = ethers.BigNumber.from("0");
+
+      expect(await walletInstanceFromOther.getBalance(BNB_INSTANCE_INDEX)).
+        to.be.deep.equal(expectedRandomAccountBalance);
+    });
+
+    it("Transfer validations", () => {
+
+      describe("Should transfer 10 BNB from account to other account", async () => {
+
+        const { walletInstance, walletInstanceFromOther, otherAccount } = await loadFixture(deployWalletWithBNBAndConnectOther);
+
+        // uint256 in solidity are mapped as BigNumber in javascript by ethers library
+        const AMOUNT_TO_TRANSFER = ethers.BigNumber.from("10");
+
+        const BNB_INSTANCE_INDEX = 0;
+
+        await walletInstance.transfer(otherAccount.address, AMOUNT_TO_TRANSFER, BNB_INSTANCE_INDEX);
+
+        expect(await walletInstanceFromOther.getBalance(BNB_INSTANCE_INDEX)).to.be.deep.equal(AMOUNT_TO_TRANSFER);
+      });
     });
   });
 });
